c8eb88e464bf5e0f6c074aab2e60a743f1bf3e4e
Sstrv <116982899+Sstrv@users.noreply.github.com>
2022-10-29 18:20:12 -0500

    Update crc.vhd

--- crc.vhd
+++ crc.vhd
@@ -25,28 +25,19 @@ process(clk)
 	begin 
 	if rising_edge(clk) then 
 		if    estados = 0 then 	
---estado 0
 			if    sel = '0' then 
---concatenar 0000
 				palabra_s <= palabra_i & "0000";
 			elsif sel = '1' then 
---concatenar el codigo crc
 				palabra_s <= palabra_i & Palabra_bin;
 			end if; 
 			estados <= estados + 1;
 		elsif estados = 1 then 	
---estado 1
 			if    palabra_s(11-n) = '0' then 
---compara si el primer numero es 0
 				palabra_s(11-n downto 7-n) <= palabra_s(11-n downto 7-n) xor "00000";
---hace un xor entre "palabra_s" y 0000
 			elsif palabra_s(11-n) = '1' then 
---compara si el primer numero es 1
 				palabra_s(11-n downto 7-n) <= palabra_s(11-n downto 7-n) xor divisor;
---hace un xor entre "palabra_s" y "divisor"
 			end if;
 			n <= n + 1;
---se define n para mover los datos 
 			if    n = 7 and sel = '0' then 
 				estados <= estados + 1;
 				n<=0;
@@ -54,24 +45,17 @@ process(clk)
 				estados <= estados + 2;
 				n<=0;
 			end if;
---se define el estado siguente cuando acabe la sentencia 
---se reinicia el n
 		elsif estados = 2 then 
---estado 2
 			palabra_sal <= palabra_i & palabra_s(3 downto 0);
---se almacena los datos en variables 
 			palabra_bin <=             palabra_s(3 downto 0);
 			error_i<='0';
---se almacena el codigo crc
 			estados     <= 0;
 		elsif estados = 3 then 
---estado 3
 			if palabra_s(3 downto 0) /= "0000" then 
 				error_i <= '1';
 			else 
 				error_i <= '0';
 			end if;
---compara si el resultante da lo esperado
 			estados <= 0;
 		end if;
 	end if;
